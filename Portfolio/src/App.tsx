7f84c131ec6301eee6a43d8b26ba871f756182d8
matiasapl <polhwein.matias@gmail.com>
2026-02-08 11:52:39 -0300

    style: :construction: Removido espacio entre secciones, introduccion de CONVENTIONAL COMMITS. vs code
    Nueva forma de crear commits, y cambio estetico en app.tsx

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -8,21 +8,21 @@ import Estudios from "./components/Estudios/Index";
 import Proyectos from "./components/Proyectos/Index";
 
 function App() {
-useEffect(() => {
-  const handleMouseMove = (e: MouseEvent) => {
-    // IMPORTANTE: Usamos clientX/Y para la posición relativa a la ventana
-    document.body.style.setProperty("--mouse-x", `${e.clientX}px`);
-    document.body.style.setProperty("--mouse-y", `${e.clientY}px`);
-  };
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      // IMPORTANTE: Usamos clientX/Y para la posición relativa a la ventana
+      document.body.style.setProperty("--mouse-x", `${e.clientX}px`);
+      document.body.style.setProperty("--mouse-y", `${e.clientY}px`);
+    };
 
-  window.addEventListener("mousemove", handleMouseMove);
-  return () => window.removeEventListener("mousemove", handleMouseMove);
-}, []);
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
 
   return (
     <div className="font-mono relative z-10 text-white">
       <NavBar />
-      <div className=" flex flex-col justify-center mb-80 scroll-mt-10">
+      <div className="flex flex-col justify-center mb-80 scroll-mt-10">
         <Inicio id="Inicio" />
       </div>
 
@@ -34,11 +34,11 @@ useEffect(() => {
         <Estudios id="Estudios" />
       </div>
 
-      <div className="min-h-[300vh] flex flex-col justify-center mb-80 scroll-mt-10">
+      <div className="flex flex-col justify-center mb-80 scroll-mt-10">
         <Skills id="Habilidades" />
       </div>
 
-      <div className="min-h-[300vh] flex flex-col justify-center mb-80 scroll-mt-10">
+      <div className="flex flex-col justify-center mb-80 scroll-mt-10">
         <SobreMi id="Sobre_MI" />
       </div>
     </div>
